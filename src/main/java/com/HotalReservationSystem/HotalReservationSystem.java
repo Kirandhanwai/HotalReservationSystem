5adb1975cc595aea978d0dd6c9468068269df0cf
Kirandhanwai <kirandhanwai123@gmail.com>
2022-03-14 08:50:38 +0530

    Added : Welcome to Hotal Reservation System Program

---
+++ src/main/java/com/HotalReservationSystem/HotalReservationSystem.java
@@ -0,0 +1,8 @@
+package com.HotalReservationSystem;
+
+public class HotalReservationSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System Program");
+	}
+
+}
